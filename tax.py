377788ec23bd303fcbf58ad87cb5a18bbd8fead0
mslizwang <amigolizwang@outlook.com>
2019-02-22 16:40:19 +0800

    Update tax.py

--- tax.py
+++ tax.py
@@ -1,6 +1,6 @@
 # -*- coding: UTF-8 -*-
 
-#获取相应的层级，税率和速算扣除数
+#获取相应的层级，税率和速算扣除数，返回一个元组
 def r_n_d(taxable_income):
     #level
     lv = [0, 36000, 144000, 300000, 420000, 660000, 960000]
@@ -37,20 +37,17 @@ def r_n_d(taxable_income):
         qd = d[6]
         return lv[6], tr, qd
 
-#print r_n_d(12000)
-#print r_n_d(12000)[0]
-#print r_n_d(12000)[1]
-#print r_n_d(12000)[2]
-
-#根据月份，确定每月的累计应税额
+#个税计算器
 def calculator(month, income, tax_free_income, security_n_fund, tax_threshold, special_deduction, other_deduction):
+    #根据月份，确定每月的累计应税额
     taxable_income_by_month = []
     i = 0
     while i < month:
         i+=1
         current_taxable_income = (income - tax_free_income - security_n_fund - tax_threshold - special_deduction - other_deduction)*i
         taxable_income_by_month.append(round(current_taxable_income,2))
-
+    #根据每月的累计应税所得额，找到相应的税率和速算扣除数，得到每月的应预扣预缴税额
+    #这里假设了每月工资相同，所有的扣除项目也都相同
     total_tax = []
     rate_by_month = []
     deduction_by_month = []
@@ -61,16 +58,19 @@ def calculator(month, income, tax_free_income, security_n_fund, tax_threshold, s
         deduction_by_month.append(current_deduction)
         total_tax_item = item * current_rate - current_deduction
         total_tax.append(round(total_tax_item,2))
-
+    #本期应预扣预缴税额 = 最后一个月的累计应预扣预缴税额-上个月的累计应预扣预缴税额
     if len(total_tax) > 1:
         paid_tax = total_tax[-2]
         current_tax = total_tax[-1]-paid_tax
     else:
         current_tax = total_tax[-1]
+    #本期应预扣预缴税额最小为0
+    if current_tax < 0:
+        current_tax = 0
 
-    print rate_by_month
-    print deduction_by_month
+    #print rate_by_month
+    #print deduction_by_month
     #print taxable_income_by_month
-    print total_tax
+    #print total_tax
     #print paid_tax
     return current_tax
